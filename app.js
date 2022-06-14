066c784f4f47e7c2c1ca30f5a1c897e211a718bf
Dattn0801 <Dattn0801>
2022-06-14 16:45:43 +0700

    add st

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv/config');
+const authJwt = require('./helpers/jwt');
+const errorHandler = require('./helpers/error-handler');
+
+app.use(cors());
+app.options('*', cors());
+
+//Midleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(authJwt());
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use(errorHandler);
+
+//Routers
+const categoriesRoutes = require('./routers/categories');
+const productsRouters = require('./routers/products');
+const usersRouters = require('./routers/users');
+const ordersRouters = require('./routers/orders');
+const res = require('express/lib/response');
+
+const api = process.env.API_URL;
+
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRouters);
+app.use(`${api}/users`, usersRouters);
+app.use(`${api}/orders`, ordersRouters);
+
+//Database connection
+//Add the connect before starting the serve <3
+mongoose
+    .connect(process.env.CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'eshop-database',
+    })
+    .then(() => {
+        console.log('Database Connection is ready');
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+//Server
+app.listen(3000, () => {
+    console.log(api);
+    console.log('server is running http://localhost:3000');
+});
---
+++ helpers/jwt.js
@@ -0,0 +1,35 @@
+const expressJwt = require('express-jwt');
+function authJwt() {
+    const secret = process.env.secret;
+    const api = process.env.API_URL;
+    return expressJwt({
+        secret,
+        algorithms: ['HS256'],
+        isRevoked: isRevoked,
+    }).unless({
+        //exclude path to logged inn <3
+        path: [
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            {
+                url: /\/api\/v1\/orders(.*)/,
+                methods: ['GET', 'OPTIONS', 'POST'],
+            },
+            `${api}/users/login`,
+            `${api}/users/register`,
+
+            //alow all path
+            //{ url: /(.*)/ },
+        ],
+    });
+}
+
+async function isRevoked(req, payload, done) {
+    if (!payload.isAdmin) {
+        done(null, true);
+    }
+    done();
+}
+
+module.exports = authJwt;
